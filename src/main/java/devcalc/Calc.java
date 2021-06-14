ee3b08ffa310cd6e89a8ec1227b3240289aeb17e
Thiago <thagoveigati@gmail.com>
2021-06-14 11:57:22 +0100

    Estrutura básica

---
+++ src/main/java/devcalc/Calc.java
@@ -0,0 +1,21 @@
+// 1 - Pacote
+package devcalc;
+// 2 - Biblioteca
+
+// - Classe
+public class Calc {
+    // - 3.1 Atribuitos
+
+    // - 3.2 - Métodos e Funções
+    public static void main(String[] args){
+        System.out.println("Passou pelo método main");
+        somarDoisNumeros();
+    }
+
+    public static void somarDoisNumeros(){
+        System.out.println("Passou pelo método somarDoisNumeros");
+    }
+
+
+
+}
