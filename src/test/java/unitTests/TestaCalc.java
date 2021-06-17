a6b54f9b92a7f7615ecbdb0669fe91ed5b0c7dc4
Thiago <thagoveigati@gmail.com>
2021-06-17 12:57:53 +0100

    Testes para soma, subtração, multiplicação e divisão conluídos, inserida opção "priority"

--- src/test/java/unitTests/TestaCalc.java
+++ src/test/java/unitTests/TestaCalc.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class TestaCalc {
-    @Test
+    @Test(priority = 1)
     public void testarSomarDoisNumeros() {
         /* 1 - Preprara - Configurar - Given */
         int num1 = 5;
@@ -19,9 +19,9 @@ public class TestaCalc {
         // 3 - Validar - Then
         System.out.println("O resultado esperado e " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
 
-        assertEquals(resultadoEsperado, resultadoAtual);
+        assertEquals(resultadoAtual, resultadoEsperado);
     }
-    @Test
+    @Test(priority = 2)
     public void testarSubtrairDoisNumeros() {
         /* 1 - Preprara - Configurar - Given */
         int num1 = 8;
@@ -34,7 +34,37 @@ public class TestaCalc {
         // 3 - Validar - Then
         System.out.println("O resultado esperado e " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
 
-        assertEquals(resultadoEsperado, resultadoAtual);
+        assertEquals(resultadoAtual, resultadoEsperado);
     }
+    @Test(priority = 3)
+    public void testarmultiplicarDoisNumeros() {
+        /* 1 - Preprara - Configurar - Given */
+        int num1 = 5;
+        int num2 = 3;
+        int resultadoEsperado = 15;
 
+        // 2 - Executar - When
+        int resultadoAtual = Calc.multiplicarDoisNumeros(num1, num2);
+
+        // 3 - Validar - Then
+        System.out.println("O resultado esperado e " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
+
+        assertEquals(resultadoAtual, resultadoEsperado);
+    }
+    @Test(priority = 3)
+    public void testardividirDoisNumeros() {
+        /* 1 - Preprara - Configurar - Given */
+        int num1 = 10;
+        int num2 = 2;
+        int resultadoEsperado = 5;
+
+        // 2 - Executar - When
+        int resultadoAtual = Calc.dividirDoisNumeros(num1, num2);
+
+        // 3 - Validar - Then
+        System.out.println("O resultado esperado e " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
+
+        assertEquals(resultadoAtual, resultadoEsperado);
+    }
 }
+
